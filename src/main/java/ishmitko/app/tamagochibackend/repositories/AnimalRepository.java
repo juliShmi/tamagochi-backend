ae2a0a554c5874002021158e169891ad531e31e3
juliShmi <juliashmitko@gmail.com>
2023-04-24 18:22:28 +0300

    Revert "Add Model, POST-Request, GET-Request, Test for POST"
    This reverts commit 7c885cc443099084c1734eb2ffe5f77502779355.

--- src/main/java/ishmitko/app/tamagochibackend/repositories/AnimalRepository.java
+++
@@ -1,7 +0,0 @@
-package ishmitko.app.tamagochibackend.repositories;
-
-import ishmitko.app.tamagochibackend.model.Animal;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AnimalRepository extends JpaRepository<Animal, Long> {
-}
